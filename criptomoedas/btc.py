190909bf0cdc4798e6817df30fede3b005095182
ooxigen <124283984+ooxigen@users.noreply.github.com>
2023-02-03 17:22:26 -0300

    🐍
    🐍

---
+++ criptomoedas/btc.py
@@ -0,0 +1,15 @@
+import requests
+import time
+
+while True:
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC&convert=BRL"
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_Pro_API_Key": "462b13fe-538b-4ca9-8730-8102a99dd1cc" # coinmarket key
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    bitcoin_price = data["data"]["BTC"]["quote"]["BRL"]["price"]
+    price_formatted = "R$ {:,.2f}".format(bitcoin_price)
+    print("O preço atual do BITCOIN é:", price_formatted)
+    time.sleep(1500)
---
+++ criptomoedas/readme.md
@@ -0,0 +1,14 @@
+# REPOSITÓRIO COM VALORES ATUAIS DE CRYPTO
+Aqui você verá vários scripts em python que consultam o valor atual em real das criptomoedas (litecoin/bitcoin/ethereum)
+
+## btc.py
+- Explicação: `a btc.py serve para encontrar o valor atual de um bitcoin em reais.`
+- [Código](btc.py)
+
+## ltc.py
+- Explicação: `a ltc.py serve para encontrar o valor atual de um bitcoin em reais.`
+- [Código](ltc.py)
+
+## eth.py
+- Explicação: `a eth.py serve para encontrar o valor atual de um bitcoin em reais.`
+- [Código](eth.py)
